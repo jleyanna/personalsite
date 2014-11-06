1d3deeeae1e74d7425f50bdb53d9155809346ab1
Steven Smith <stsmith@nabancard.com>
2014-11-06 00:26:30 -0500

    Adding user controller, migrate controller, and usermanage view

---
+++ app/controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+class UserController extends Controller
+{
+    /**
+     * Setup the layout used by the controller.
+     *
+     * @return void
+     */
+    public function showManage() {
+        $user = Auth::user();
+        $username = $user->username;
+        $email = $user->email;
+        $firstname = $user->firstname;
+        $lastname = $user->lastname;
+        return View::make('usermanage',array('email'=>$email,'firstname'=>$firstname,'lastname'=>$lastname,'username'=>$username));
+    }
+
+    public function postManage() {
+        $user = Auth::user();
+        if (Input::has('email'))
+        {
+            $email = Input::get('email');
+            $user->email = $email;
+        }
+        if (Input::has('password') && Input::has('confirmpassword'))
+        {
+            $password = Input::get('password');
+            $confirmpassword = Input::get('confirmpassword');
+            if ($password === $confirmpassword)
+            {
+                $user->password = Hash::make($confirmpassword);
+            }
+        }
+        $user->save();
+        return Redirect::route('manageuser');
+    }
+
+}
